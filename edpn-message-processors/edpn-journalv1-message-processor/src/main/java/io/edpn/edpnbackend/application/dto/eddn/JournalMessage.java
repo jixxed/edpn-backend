accc6853ca16d08d5f902417aaeedf66a00a5c62
RexTheCapt <6693554+RexTheCapt@users.noreply.github.com>
2023-05-06 17:51:24 +0200

    Deserialized Journal V1

--- edpn-message-processors/edpn-journalv1-message-processor/src/main/java/io/edpn/edpnbackend/application/dto/eddn/JournalMessage.java
+++ edpn-message-processors/edpn-journalv1-message-processor/src/main/java/io/edpn/edpnbackend/application/dto/eddn/JournalMessage.java
@@ -27,56 +27,20 @@ public interface JournalMessage {
         @Data
         @NoArgsConstructor
         public static class Message {
-            @JsonProperty("systemName")
-            private String systemName;
-            @JsonProperty("stationName")
-            private String stationName;
-            @JsonProperty("marketId")
-            private long marketId;
-            @JsonProperty("horizons")
-            private boolean horizons;
-            @JsonProperty("odyssey")
-            private boolean odyssey;
             @JsonProperty("timestamp")
             private String timestamp;
-            @JsonProperty("economies")
-            private Economy[] economies;
-            @JsonProperty("prohibited")
-            private String[] prohibited;
-            @JsonProperty("commodities")
-            private Commodity[] commodities;
-        }
-
-        @Data
-        @NoArgsConstructor
-        public static class Commodity {
-            @JsonProperty("name")
-            private String name;
-            @JsonProperty("meanPrice")
-            private int meanPrice;
-            @JsonProperty("buyPrice")
-            private int buyPrice;
-            @JsonProperty("stock")
-            private int stock;
-            @JsonProperty("stockBracket")
-            private int stockBracket;
-            @JsonProperty("sellPrice")
-            private int sellPrice;
-            @JsonProperty("demand")
-            private int demand;
-            @JsonProperty("demandBracket")
-            private int demandBracket;
-            @JsonProperty("statusFlags")
-            private String[] statusFlags;
-        }
-
-        @Data
-        @NoArgsConstructor
-        public static class Economy {
-            @JsonProperty("name")
-            private String name;
-            @JsonProperty("proportion")
-            private double proportion;
+            @JsonProperty("event")
+            private String event;
+            @JsonProperty("horizons")
+            private Boolean horizons;
+            @JsonProperty("odyssey")
+            private Boolean odyssey;
+            @JsonProperty("StarSystem")
+            private String starSystem;
+            @JsonProperty("StarPos")
+            private Double[] starPos;
+            @JsonProperty("SystemAddress")
+            private Long systemAddress;
         }
     }
 }

c102821d406eb790d2f45da89b4a57a11a0ae39e
RexTheCapt <6693554+RexTheCapt@users.noreply.github.com>
2023-05-07 14:51:21 +0200

    Removed not needed classes

--- message-processors/approachsettlementv1-processor/src/main/java/io/edpn/backend/messageprocessor/commodityv3/application/usecase/AnomalousCommodityDataDetectionUseCase.java
+++
@@ -1,7 +0,0 @@
-package io.edpn.backend.messageprocessor.commodityv3.application.usecase;
-
-import io.edpn.backend.messageprocessor.commodityv3.application.dto.persistence.HistoricStationCommodityMarketDatumEntity;
-
-public interface AnomalousCommodityDataDetectionUseCase {
-    boolean isAnomalous(HistoricStationCommodityMarketDatumEntity commodityMarketDatumEntity);
-}
